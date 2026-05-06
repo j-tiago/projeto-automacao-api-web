50d18a603a22f8fc99a4d5ca4e0f8605f47bcb5f
Seu Nome <jtiagolimac@gmail.com>
2026-05-06 03:16:02 -0300

    Correção webdriver, configuração do pytest e testes E2E/API finalizados

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,29 +1,25 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture()
 def setup_teardown():
-    # 1. Instala e configura o serviço do ChromeDriver automaticamente
-    servico = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     
-    # 2. Configurações essenciais para rodar na Pipeline (GitHub Actions / Linux)
-    # options.add_argument('--headless') # Roda o navegador em segundo plano (sem interface gráfica)
+    # 1. Configurações essenciais para rodar na Pipeline (GitHub Actions / Linux)
+    options.add_argument('--headless') # Roda o navegador em segundo plano (sem interface gráfica)
     options.add_argument('--no-sandbox') # Necessário para rodar no Ubuntu do GitHub Actions
     options.add_argument('--disable-dev-shm-usage') # Evita problemas de limite de memória no servidor
     options.add_argument('--window-size=1920,1080') # Garante o tamanho da tela para não esconder botões
     
-    # 3. Inicializa o navegador com as opções configuradas
-    driver = webdriver.Chrome(service=servico, options=options)
+    # 2. Inicializa o navegador com as opções configuradas
+    driver = webdriver.Chrome(options=options)
     
-    # 4. Configura uma espera implícita (espera até 5s para um elemento aparecer antes de dar erro)
+    # 3. Configura uma espera implícita (espera até 5s para um elemento aparecer antes de dar erro)
     driver.implicitly_wait(5)
     driver.maximize_window()
     
-    # 5. Entrega o driver para o teste utilizar
+    # 4. Entrega o driver para o teste utilizar
     yield driver 
     
-    # 6. Teardown: Fecha o navegador e encerra o processo após o fim do teste
+    # 5. Teardown: Fecha o navegador e encerra o processo após o fim do teste
     driver.quit()
