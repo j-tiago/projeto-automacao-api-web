ab9cb288b849c23c4b75ba7174e8ba06394a4efa
Seu Nome <jtiagolimac@gmail.com>
2026-05-06 21:24:20 -0300

    Atualizando config do driver para headless=new e user-agent para evitar timeout no github

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,21 +5,23 @@ from selenium import webdriver
 def setup_teardown():
     options = webdriver.ChromeOptions()
     
-    # 1. Configurações essenciais para rodar na Pipeline (GitHub Actions / Linux)
-    options.add_argument('--headless') # Roda o navegador em segundo plano (sem interface gráfica)
-    options.add_argument('--no-sandbox') # Necessário para rodar no Ubuntu do GitHub Actions
-    options.add_argument('--disable-dev-shm-usage') # Evita problemas de limite de memória no servidor
-    options.add_argument('--window-size=1920,1080') # Garante o tamanho da tela para não esconder botões
+    # --- O Segredo para CI/CD no GitHub Actions ---
+    # Usa o novo motor headless do Chrome, idêntico a um monitor real
+    options.add_argument('--headless=new') 
     
-    # 2. Inicializa o navegador com as opções configuradas
+    options.add_argument('--no-sandbox') 
+    options.add_argument('--disable-dev-shm-usage') 
+    options.add_argument('--window-size=1920,1080')
+    options.add_argument('--disable-gpu') # Dá mais estabilidade no Ubuntu
+    
+    # Disfarça o robô simulando um usuário real no Windows
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
+    
     driver = webdriver.Chrome(options=options)
     
-    # 3. Configura uma espera implícita (espera até 5s para um elemento aparecer antes de dar erro)
     driver.implicitly_wait(5)
     driver.maximize_window()
     
-    # 4. Entrega o driver para o teste utilizar
     yield driver 
     
-    # 5. Teardown: Fecha o navegador e encerra o processo após o fim do teste
     driver.quit()
